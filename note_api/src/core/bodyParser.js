dbebf2798ea9b729cac4cd366da9d61a828cbc38
sajalmondal-gub <sajalmondal.gub@gmail.com>
2026-07-04 21:45:41 +0600

    Implement body parser with size limit and JSON handling

--- note_api/src/core/bodyParser.js
+++ note_api/src/core/bodyParser.js
@@ -5,4 +5,34 @@ module.expots = async function bodyParse(req, res, next) {
     req.body = {};
     return await next();
   }
+  return new Promise((resolve) => {
+    let body = "";
+    const MAX_SIZE = 2 * 1204 * 1204; //size declear
+
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+      if (body.length > MAX_SIZE) {
+        res.sendJSON(413, {
+          error: "Payload Too Large",
+          message: "Max body limit is 2MB",
+        });
+        req.destroy();
+        resolve();
+      }
+    });
+    req.on("end", async () => {
+      try {
+        const contentType = req.headers["content-type"] || "";
+        if (contentType.includes("application/json")) {
+          req.body = body ? JSON.parse(body) : {};
+        } else {
+          req.body = body;
+        }
+        resolve(awaitnext());
+      } catch (error) {
+        res.sendJSON(400, { error: "Malformed JSON Payload" });
+        resolve();
+      }
+    });
+  });
 };
