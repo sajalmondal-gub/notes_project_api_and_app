e6cb8a94521b615e56600813f0daa66a9e120dbc
sajalmondal-gub <sajalmondal.gub@gmail.com>
2026-07-06 12:06:08 +0600

    Refactor error middleware: improve error logging structure and enhance response formatting for better clarity

--- note_api/src/middlewares/error.middleware.js
+++ note_api/src/middlewares/error.middleware.js
@@ -1,15 +1,24 @@
 import config from "../config/env.js";
 
 export default async function errorMiddleware(err, req, res) {
-  console.error(`[SYSTEM ERROR]`, err);
-
   const statusCode = err.statusCode ?? 500;
+  if (statusCode >= 500) {
+    console.error(`[SYSTEM ERROR]`, err);
+  } else if (config.NODE_ENV === "development") {
+    console.warn(
+      `⚠️ [Client Error] ${req.method} ${req.path} - Status: ${statusCode} - Message: ${err.message}`,
+    );
+  }
+  
   if (res.writableEnded) return;
 
-  res.sendJSON(statusCode, {
+  const errorResponse = {
     success: false,
     error: err.name || "InternalServerError",
     message: err.message || "An unexpected system fault occurred",
-    ...(config.NODE_ENV === "development" && { stack: err.stack }),
-  });
+  };
+  if (err.errors && err.errors.length > 0) {
+    errorResponse.errors = err.errors;
+  }
+  res.sendJSON(statusCode, errorResponse);
 }
