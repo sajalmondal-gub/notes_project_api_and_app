2f083ee6df3653f7093818d2eab7c47506a8cc74
sajalmondal-gub <sajalmondal.gub@gmail.com>
2026-07-06 20:34:52 +0600

    Fix typo in Users model: correct 'eamil' to 'email' for proper user data handling; add migration for reset password functionality

--- note_api/src/models/user.model.js
+++ note_api/src/models/user.model.js
@@ -1,8 +1,8 @@
 class Users {
-  constructor({ id, name, eamil, password, created_at, updated_at }) {
+  constructor({ id, name, email, password, created_at, updated_at }) {
     this.id = id;
     this.name = name;
-    this.eamil = eamil;
+    this.eamil = email;
     this.password = password;
     this.created_at = created_at;
     this.updated_at = updated_at;
