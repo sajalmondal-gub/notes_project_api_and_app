50564eba1def2293cf9eff140e85dc7e5e57a89a
sajalmondal-gub <sajalmondal.gub@gmail.com>
2026-07-07 19:02:06 +0600

    Add nodemailer dependency and update Users model to include reset password fields

--- note_api/src/models/user.model.js
+++ note_api/src/models/user.model.js
@@ -1,9 +1,20 @@
 class Users {
-  constructor({ id, name, email, password, created_at, updated_at }) {
+  constructor({
+    id,
+    name,
+    email,
+    password,
+    created_at,
+    updated_at,
+    reset_password_token,
+    reset_password_expires,
+  }) {
     this.id = id;
     this.name = name;
     this.eamil = email;
     this.password = password;
+    this.reset_password_expires = reset_password_expires;
+    this.reset_password_token = reset_password_token;
     this.created_at = created_at;
     this.updated_at = updated_at;
   }
