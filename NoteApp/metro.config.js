e75f66255811f55aee414f21e3850d1cd567d199
sajalmondal-gub <sajalmondal.gub@gmail.com>
2026-07-12 21:24:34 +0600

    refactor: remove NativeWind integration and restore Metro config structure

--- NoteApp/metro.config.js
+++ NoteApp/metro.config.js
@@ -1,5 +1,4 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-const { withNativeWind } = require('nativewind/metro');
 
 /**
  * Metro configuration
@@ -7,7 +6,13 @@ const { withNativeWind } = require('nativewind/metro');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    blockList: [
+      // Ignore Android C++ temporary build files that crash the watcher
+      /.*\/android\/\.cxx\/.*/
+    ],
+  },
+};
 
-module.exports = withNativeWind(mergeConfig(getDefaultConfig(__dirname), config), { input: './global.css' });
-
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
